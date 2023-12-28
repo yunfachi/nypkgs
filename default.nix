7d239e8a906009f0ed758bd4011bbefea010dfa5
yunfachi <yunfachi@gmail.com>
2023-12-28 19:36:41 +0000

    feat: add sops-decrypt

--- default.nix
+++ default.nix
@@ -8,8 +8,14 @@
   };
 in {
   canary = pkgs.callPackage ./pkgs/canary {};
-  yunfaavatar = pkgs.callPackage ./pkgs/yunfaavatar {};
   shikimori = pkgs.callPackage ./pkgs/shikimori {};
+  yunfaavatar = pkgs.callPackage ./pkgs/yunfaavatar {};
 
+  sops-decrypt = args:
+    import ./lib/sops-decrypt.nix {
+      inherit (pkgs) sops age;
+      inherit (pkgs.stdenvNoCC) mkDerivation;
+      inherit (args) path publicAge privateAgeFile;
+    };
   umport = import ./lib/umport.nix;
 }
---
+++ lib/sops-decrypt.nix
@@ -0,0 +1,29 @@
+{
+  path,
+  publicAge,
+  privateAgeFile,
+  age ? (import <nixpkgs> {}).age,
+  sops ? (import <nixpkgs> {}).sops,
+  mkDerivation ? (import <nixpkgs> {}).stdenvNoCC.mkDerivation,
+}: let
+  privateAgeFileModule = mkDerivation {
+    name = "privateAgeFile";
+    src = privateAgeFile + "/..";
+    buildPhase = ''
+      mkdir $out
+      cp ${builtins.baseNameOf privateAgeFile} $out/keys.txt
+    '';
+  };
+in
+  (mkDerivation {
+    name = "sops-decrypt-${builtins.baseNameOf path}";
+    src = path + "/..";
+    env = {
+      SOPS_AGE_KEY_FILE = privateAgeFileModule + "/keys.txt";
+    };
+    buildInputs = [age sops];
+    buildPhase = ''
+      sops --age "${publicAge}" --decrypt "./${builtins.baseNameOf path}" > "$out"
+    '';
+  })
+  .outPath
