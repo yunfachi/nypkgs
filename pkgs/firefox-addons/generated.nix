8a001a56d7bcebd772f99fb1f0fdcbaaa3e1f5dc
yunfachi <yunfachi@gmail.com>
2024-02-03 12:40:41 +0000

    feat(firefox-addons): add shikiplayer

--- pkgs/firefox-addons/generated.nix
+++ pkgs/firefox-addons/generated.nix
@@ -15,4 +15,24 @@
         platforms = platforms.all;
       };
     };
+    "shikiplayer" = buildFirefoxXpiAddon {
+      pname = "shikiplayer";
+      version = "5.4.0";
+      addonId = "{e6624e6b-2351-4a1d-b7b7-c714fffee424}";
+      url = "https://addons.mozilla.org/firefox/downloads/file/4226497/shikiplayer-5.4.0.xpi";
+      sha256 = "0d73515982b273e1ce01795922703bc55124ac10ff78450b8987ec306e12ac6a";
+      meta = with lib;
+      {
+        homepage = "https://github.com/qt-kaneko/Shikiplayer";
+        description = "Adds Kodik player to Shikimori website";
+        mozPermissions = [
+          "*://kodikapi.com/*"
+          "*://raw.githubusercontent.com/*"
+          "*://shikimori.me/*"
+          "*://shikimori.one/*"
+          "*://kodik.info/*"
+        ];
+        platforms = platforms.all;
+      };
+    };
   }
