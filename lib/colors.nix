5df6f96755991cf8f8f3f3beaa01e386ae02ff2f
yunfachi <yunfachi@gmail.com>
2024-02-14 21:29:56 +0300

    refactor(colors): remove __toString where it is not required

--- lib/colors.nix
+++ lib/colors.nix
@@ -1,4 +1,8 @@
-{ylib, lib, ...}: {
+{
+  ylib,
+  lib,
+  ...
+}: {
   color = color: let
     regex = builtins.match "^#?([0-9a-fA-F]{6})$" color;
     valid = regex == null;
@@ -6,28 +10,13 @@
     value = builtins.elemAt regex 0;
     __toString = _: value; # "FFFFFF"
 
-    number = {
-      value = toString (ylib.hexToDec value);
-      __toString = self: self.value; # "16777215"
-    };
+    number = toString (ylib.hexToDec value); # "16777215"
 
-    hex = {
-      value = "#${value}";
-      __toString = self: self.value; # "#FFFFFF"
-    };
-    prefixHex = {
-      value = "0x${value}";
-      __toString = self: self.value; # "0xFFFFFF"
-    };
+    hex = "#${value}"; # "#FFFFFF"
+    prefixHex = "0x${value}"; # "0xFFFFFF"
 
     rgbList = map (start: toString (ylib.hexToDec (builtins.substring start 2 value))) [0 2 4]; # [255, 255, 255]
-    rgbSep = sep: {
-      value = lib.concatStringsSep sep rgbList;
-      __toString = self: self.value; # "255<sep>255<sep>255"
-    };
-    rgb = {
-      value = rgbSep ",";
-      __toString = self: self.value; # "255,255,255"
-    };
+    rgbSep = sep: lib.concatStringsSep sep rgbList; # "255<sep>255<sep>255"
+    rgb = rgbSep ","; # "255,255,255"
   };
 }
