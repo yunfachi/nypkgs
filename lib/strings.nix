2b7afa178d5daee226f27bfd9f317718f732ac4f
yunfachi <yunfachi@gmail.com>
2024-03-23 13:55:02 +0300

    feat(lib): add strippedHasSuffix and strippedRemoveSuffix

--- lib/strings.nix
+++ lib/strings.nix
@@ -1,4 +1,17 @@
-{lib, ...}: {
+{lib, ...}:
+with lib; rec {
+  strippedHasSuffix = suffix: string:
+    hasSuffix suffix (replaceStrings [" "] [""] string);
+
+  strippedRemoveSuffix = suffix: string: let
+    splitted = splitString suffix string;
+  in
+    lib.concatStrings (
+      if ((lib.replaceStrings [" "] [""] (lib.last splitted)) == "")
+      then lib.lists.init splitted
+      else splitted
+    );
+
   concatLinesWithSemicolon = command:
     lib.concatStrings (builtins.map (line:
       if (lib.hasSuffix ";" line || line == "")
