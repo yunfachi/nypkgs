85d4ced4283739adf684020e013db0a83966760a
yunfachi <yunfachi@gmail.com>
2024-03-28 10:31:00 +0300

    feat(path): add readCanonicalFile

--- lib/path.nix
+++ lib/path.nix
@@ -1,16 +1,22 @@
 {pkgs, ...}: rec {
-  pathToStore = path:
+  _pathToStore = path:
     (pkgs.linkFarm "path2store" [
       {
         name = "path";
         inherit path;
       }
     ])
-    .outPath
-    + "/path";
+    .outPath;
+  pathToStore = path: (_pathToStore path + "/path");
 
   /*
   read file by absolute path in pure mode
   */
   readAbsoluteFile = path: builtins.readFile (pathToStore path);
+  readCanonicalFile = path:
+    builtins.readFile (pkgs.stdenv.mkDerivation {
+      name = "readCanonicalFile";
+      src = _pathToStore (/. + path);
+      buildPhase = ''cat path > $out'';
+    });
 }
