193c13630997d000e72e9ae6f6bfe9b71f5c4b3f
yunfachi <yunfachi@gmail.com>
2025-10-25 17:08:48 +0300

    lib/sops: only local build

--- lib/sops.nix
+++ lib/sops.nix
@@ -5,48 +5,56 @@
   lib,
   linkFarm,
   ...
-}: {
-  sops-decrypt = {
-    path,
-    publicAge ? null,
-    privateKeysFile,
-    key ? "",
-  }:
-    (stdenvNoCC.mkDerivation {
+}:
+{
+  sops-decrypt =
+    {
+      path,
+      publicAge ? null,
+      privateKeysFile,
+      key ? "",
+    }:
+    stdenvNoCC.mkDerivation {
       name = "sops-decrypt-${builtins.baseNameOf path}";
-      src = linkFarm "sops-decrypt-src" (
-        [
-          {
-            name = baseNameOf path;
-            inherit path;
-          }
-          {
-            name = "keys.txt";
-            path = privateKeysFile;
-          }
-        ]
-        #++ (
-        #  if (builtins.pathExists (path + "/../.sops.yaml"))
-        #  then [
-        #    {
-        #      name = "sops.yaml";
-        #      path = path + "/../.sops.yaml";
-        #    }
-        #  ]
-        #  else []
-        #)
+      src = linkFarm "sops-decrypt-src" ([
+        {
+          name = baseNameOf path;
+          inherit path;
+        }
+        {
+          name = "keys.txt";
+          path = privateKeysFile;
+        }
+      ]
+      #++ (
+      #  if (builtins.pathExists (path + "/../.sops.yaml"))
+      #  then [
+      #    {
+      #      name = "sops.yaml";
+      #      path = path + "/../.sops.yaml";
+      #    }
+      #  ]
+      #  else []
+      #)
       );
-      buildInputs = [age sops];
-      buildPhase = with lib; let
-        ageArg = optionalString (publicAge != null) "--age '${publicAge}'";
-        extractArg = optionalString (key != "") "--extract '${
-          lib.concatMapStrings (x: ''["${x}"]'') (lib.splitString "/" key)
-        }'";
-      in ''
-        export SOPS_AGE_KEY_FILE=keys.txt;
+      buildInputs = [
+        age
+        sops
+      ];
+      buildPhase =
+        with lib;
+        let
+          ageArg = optionalString (publicAge != null) "--age '${publicAge}'";
+          extractArg =
+            optionalString (key != "")
+              "--extract '${lib.concatMapStrings (x: ''["${x}"]'') (lib.splitString "/" key)}'";
+        in
+        ''
+          export SOPS_AGE_KEY_FILE=keys.txt;
 
-        sops ${ageArg} ${extractArg} --decrypt "./${baseNameOf path}" > $out
-      '';
-    })
-    .outPath;
+          sops ${ageArg} ${extractArg} --decrypt "./${baseNameOf path}" > $out
+        '';
+      preferLocalBuild = true;
+      allowSubstitutes = false;
+    };
 }
